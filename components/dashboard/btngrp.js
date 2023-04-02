a9c95604c87b7e63dd6a6758cd15519fe8f59039
Arsalan Bashir <arsalanbashir831@gmail.com>
2023-04-02 21:31:38 +0500

    set

--- components/dashboard/btngrp.js
+++ components/dashboard/btngrp.js
@@ -1,8 +1,7 @@
 function Btngrp() {
     return ( <>
-    
-    <button type="button" class="btn btn-success">Accept</button ><button type="button" class="btn btn-danger">Reject</button>
-    
+    <button type="button" class="btn btn-success">Accept</button >
+    <button type="button" class="btn btn-danger">Reject</button>
     </> );
 }
 
